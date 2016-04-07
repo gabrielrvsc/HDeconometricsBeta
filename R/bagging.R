9e64fa28072a40c8c599816064d1ab123cc9e4a6
gabrielrvsc <gabrielrvsc@yahoo.com.br>
2016-04-07 17:07:05 -0300

    Bagging Commit 1 	new file:   R/bagging.R 	new file:   R/baggit.R 	new file:   man/bagging.Rd 	new file:   man/baggit.Rd

---
+++ R/bagging.R
@@ -0,0 +1,18 @@
+
+bagging=function(y,X,fn=NULL,R=100,l=5,sim="fixed",pre.testing="joint",fixed.controls=NULL,...){
+  
+  tsd=cbind(y,X)
+  if(pre.testing=="personal"){
+    bag=tsboot(tseries=tsd,statistic=fn,R=R,l=l
+               ,sim=sim, ...)
+  }else{
+    bag=tsboot(tseries=tsd,statistic=baggit,R=R,l=l
+               ,sim=sim, pre.testing=pre.testing, fixed.controls=fixed.controls,... )
+  }
+  
+  original=bag$t0
+  bootres=bag$t
+  colnames(bootres)=names(original)
+  
+  return(list("boot.coef"=bootres,orig.coef="original"))
+}
