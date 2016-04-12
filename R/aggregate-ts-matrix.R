2bd80e040826664ecdf2de1462386e6f989fc755
gabrielrvsc <gabrielrvsc@yahoo.com.br>
2016-04-12 15:10:56 -0300

    Update aggregate-ts-matrix.R

--- R/aggregate-ts-matrix.R
+++ R/aggregate-ts-matrix.R
@@ -2,17 +2,17 @@
 aggregate.ts.matrix=function(matrix,aggregation=c("month","week"),FUN=sum,dates=NULL){
 
   if(length(dates)==0){
-    dates=as.Date(row.names(final.table))
+    dates=as.Date(row.names(matrix))
   }
-  if(is.zoo(tsm)==FALSE){
+  if(is.zoo(matrix)==FALSE){
     matrix=zoo(matrix,dates)
   }
   if(aggregation=="month"){
     month = function(x)format(x, '%Y.%m')
-    tsm=aggregate(tsm, by=month, FUN=FUN)
+    tsm=aggregate(matrix, by=month, FUN=FUN)
   }else{
     week <- function(x)format(x, '%Y.%W')
-    tsm=aggregate(tsm, by=week, FUN=FUN)
+    tsm=aggregate(matrix, by=week, FUN=FUN)
   }
   return(tsm)
 }
