165289e909774e19716bf287da1f07e15587665e
gabrielrvsc <gabrielrvsc@yahoo.com.br>
2016-04-13 12:35:10 -0300

    Update aggregate-ts-matrix.R

--- R/aggregate-ts-matrix.R
+++ R/aggregate-ts-matrix.R
@@ -2,6 +2,15 @@
 aggregate.ts.matrix=function (matrix, aggregation = c("month", "week"), FUN = sum, 
           dates = NULL) 
 {
+  
+  func=function(x){
+    if(all(is.na(x))){
+      return(NA)
+    }else{
+      FUN(x,na.rm=TRUE)
+    }
+  }
+  
   if (length(dates) == 0) {
     dates = as.Date(row.names(matrix))
   }
@@ -10,11 +19,11 @@ aggregate.ts.matrix=function (matrix, aggregation = c("month", "week"), FUN = su
   }
   if (aggregation == "month") {
     month = function(x) format(x, "%Y.%m")
-    tsm = aggregate(matrix, by = month, FUN = function(x) FUN(x,na.rm=TRUE))#, na.action = na.pass)
+    tsm = aggregate(matrix, by = month, FUN = func)#, na.action = na.pass)
   }
   else {
     week <- function(x) format(x, "%Y.%W")
-    tsm = aggregate(matrix, by = week, FUN = function(x) FUN(x,na.rm=TRUE))#, na.action = na.pass)
+    tsm = aggregate(matrix, by = week, FUN = func)#, na.action = na.pass)
   }
   return(tsm)
 }
