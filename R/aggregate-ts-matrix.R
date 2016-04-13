21333d8d5ea49100c5e52122ffded8e82856c505
gabrielrvsc <gabrielrvsc@yahoo.com.br>
2016-04-13 12:25:55 -0300

    Update aggregate-ts-matrix.R

--- R/aggregate-ts-matrix.R
+++ R/aggregate-ts-matrix.R
@@ -1,18 +1,20 @@
 
-aggregate.ts.matrix=function(matrix,aggregation=c("month","week"),FUN=sum,dates=NULL){
-
-  if(length(dates)==0){
-    dates=as.Date(row.names(matrix))
+aggregate.ts.matrix=function (matrix, aggregation = c("month", "week"), FUN = sum, 
+          dates = NULL) 
+{
+  if (length(dates) == 0) {
+    dates = as.Date(row.names(matrix))
   }
-  if(is.zoo(matrix)==FALSE){
-    matrix=zoo(matrix,dates)
+  if (is.zoo(matrix) == FALSE) {
+    matrix = zoo(matrix, dates)
   }
-  if(aggregation=="month"){
-    month = function(x)format(x, '%Y.%m')
-    tsm=aggregate(matrix, by=month, FUN=FUN)
-  }else{
-    week <- function(x)format(x, '%Y.%W')
-    tsm=aggregate(matrix, by=week, FUN=FUN)
+  if (aggregation == "month") {
+    month = function(x) format(x, "%Y.%m")
+    tsm = aggregate(matrix, by = month, FUN = function(x) FUN(x,na.rm=TRUE))#, na.action = na.pass)
+  }
+  else {
+    week <- function(x) format(x, "%Y.%W")
+    tsm = aggregate(matrix, by = week, FUN = function(x) FUN(x,na.rm=TRUE))#, na.action = na.pass)
   }
   return(tsm)
 }
