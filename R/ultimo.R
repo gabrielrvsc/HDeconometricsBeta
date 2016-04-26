c392a28c3606ac598d872c35b6cc158b6afc1d43
gabrielrvsc <gabrielrvsc@yahoo.com.br>
2016-04-26 15:58:18 -0300

    Create ultimo.R

---
+++ R/ultimo.R
@@ -0,0 +1,8 @@
+ultimo = function(x, na.rm = TRUE) {
+        if (all(is.na(x))) {
+            return(NA)
+        }
+        else {
+            aux = which(is.na(x) == FALSE)
+            return(x[aux[length(aux)]])
+        }
